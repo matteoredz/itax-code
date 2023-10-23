a5580d84b4fb31e788a904b7abd4b0d23e9c62d7
Matteo Rossi <mttrss5@gmail.com>
2023-10-23 16:44:24 +0200

    chore: enable SimpleCov branch coverage

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,6 +3,8 @@
 require "simplecov"
 SimpleCov.start do
   add_filter "test"
+  enable_coverage :branch
+  minimum_coverage line: 100, branch: 100
 end
 
 $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
